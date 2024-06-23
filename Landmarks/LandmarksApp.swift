80ad085d79c678038ff31f560e0c9c98e646e0b9
viccarre <viccarre@gmail.com>
2024-06-22 18:47:54 -0600

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Victor Carreno on 22/06/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LandmarksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
