2285258d5a69e9d9e0ba64a83b4ed6cf3115782f
viccarre <viccarre@gmail.com>
2024-06-27 00:18:24 -0600

    Customize the row preview

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -22,6 +22,21 @@ struct LandmarkRow: View {
     }
 }
 
+/*
+ Having the two previews separated
+ #Preview("Turtle Rock") {
+     LandmarkRow(landmark: landmarks[0])
+ }
+
+ #Preview("Salmon") {
+     LandmarkRow(landmark: landmarks[1])
+ }
+ 
+ */
+
 #Preview {
-    LandmarkRow(landmark: landmarks[0])
+    Group{
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
