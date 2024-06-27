29848da75dd8749cefc8bde6f30525a9683f64c5
viccarre <viccarre@gmail.com>
2024-06-27 00:12:34 -0600

    Create the row view

---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Victor Carreno on 23/06/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion{
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.116_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    }
+}
+
+#Preview {
+    MapView()
+}
