d7ba44eac9393b47f9ec64441ff2c7f5d63fbc75
viccarre <viccarre@gmail.com>
2024-07-03 01:42:58 -0600

    Add a control to toggle the state

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -16,16 +16,27 @@ struct LandmarkList: View {
         }
     }
     
+    /*
+     To combine static and dynamic views in a list, or to combine two or more different
+     groups of dynamic views, use the ForEach type instead of passing your collection of
+     data to List.
+     */
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
                 
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
